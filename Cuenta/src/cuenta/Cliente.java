ced16acd930fb9aefd93f50a6b8e7ffcac181c45
aRCe <>
2024-11-26 22:57:03 -0500

    cliente

---
+++ Cuenta/src/cuenta/Cliente.java
@@ -0,0 +1,18 @@
+package cuenta;
+//aRCe
+public class Cliente {
+    private String nombre;
+    private int Cedula;
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setCedula(int Cedula) {
+        this.Cedula = Cedula;
+    }
+    public int getCedula() {
+        return Cedula;
+    }   
+}
