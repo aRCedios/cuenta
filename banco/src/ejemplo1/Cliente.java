79dab3a8d73d6bc618c5c8679b9fa5bb65934f6c
ospenagos06 <118693261+ospenagos06@users.noreply.github.com>
2024-11-26 22:55:50 -0500

    Add files via upload

---
+++ banco/src/ejemplo1/Cliente.java
@@ -0,0 +1,11 @@
+package cuenta;
+//Oscar Penagos
+public class Cliente {
+    private String nombre;
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+}
